533d8f152cb203bdbaa034e22a416767b10697aa
brkcllakyz21 <37548000+brkcllakyz21@users.noreply.github.com>
2018-10-20 01:17:38 +0300

    Add files via upload

---
+++ 1B Array Example/1B Array Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1B_Array_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Double[] notlar = new double[50];
+            int sayaç = 0;
+
+            for (int i = 0; i < notlar.Length; i++)
+            {
+                Console.WriteLine((i + 1) + ". öğrenci için notu giriniz");
+                notlar[i] = Convert.ToDouble(Console.ReadLine());
+                if (notlar[i] > 50)
+                {
+                    sayaç++;
+                }
+            }
+            Console.WriteLine(sayaç + "kadar öğrenci başarılı oldu.");
+
+
+
+        }
+    }
+}
